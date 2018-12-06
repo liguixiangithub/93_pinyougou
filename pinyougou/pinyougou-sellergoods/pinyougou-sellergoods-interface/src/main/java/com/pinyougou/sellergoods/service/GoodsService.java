764b5312069b220f8945221f286d3500d00905c8
liguixian <rsjylgx@aliyun.com>
2018-12-06 22:49:49 +0800

    第六次提交 商品录入1
    1.基本功能
    2.商品介绍
    3.FastDFS新技术学习
    4.FastDFS的应用，商品图片上传
    
    Linux安装开启FastDFS

--- pinyougou/pinyougou-sellergoods/pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/GoodsService.java
+++ pinyougou/pinyougou-sellergoods/pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/GoodsService.java
@@ -2,9 +2,17 @@ package com.pinyougou.sellergoods.service;
 
 import com.pinyougou.pojo.TbGoods;
 import com.pinyougou.service.BaseService;
+import com.pinyougou.vo.Goods;
 import com.pinyougou.vo.PageResult;
 
 public interface GoodsService extends BaseService<TbGoods> {
 
     PageResult search(Integer page, Integer rows, TbGoods goods);
+
+    /**
+     * 保存商品基本、描述、sku信息
+     * @param goods 商品基本、描述、sku信息
+     * @return
+     */
+    void addGoods(Goods goods);
 }
