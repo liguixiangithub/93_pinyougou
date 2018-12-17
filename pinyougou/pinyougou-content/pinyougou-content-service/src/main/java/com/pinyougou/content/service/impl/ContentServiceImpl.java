49b56cda839ee463c7c00bce4790cc9abd109ce4
liguixian <rsjylgx@aliyun.com>
2018-12-17 19:41:35 +0800

    第九次提交
    1.Spring Data Redis的学习
    单机版redis && 集群版redis
    
    2.整合redis
    
    3.网站首页
    广告缓存（增删改）

--- pinyougou/pinyougou-content/pinyougou-content-service/src/main/java/com/pinyougou/content/service/impl/ContentServiceImpl.java
+++ pinyougou/pinyougou-content/pinyougou-content-service/src/main/java/com/pinyougou/content/service/impl/ContentServiceImpl.java
@@ -10,9 +10,13 @@ import com.pinyougou.content.service.ContentService;
 import com.pinyougou.service.impl.BaseServiceImpl;
 import com.pinyougou.vo.PageResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.StringUtils;
 import tk.mybatis.mapper.entity.Example;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service(interfaceClass = ContentService.class)
@@ -20,6 +24,12 @@ public class ContentServiceImpl extends BaseServiceImpl<TbContent> implements Co
 
     @Autowired
     private ContentMapper contentMapper;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    //在redis中内容对应的key
+    private static final String REDIS_CONTENT = "content";
 
     @Override
     public PageResult search(Integer page, Integer rows, TbContent content) {
@@ -40,15 +50,84 @@ public class ContentServiceImpl extends BaseServiceImpl<TbContent> implements Co
     @Override
     public List<TbContent> findContentListByCategoryId(Long categoryId) {
 
+        List<TbContent> list = null;
+
+        try {
+            //先从缓存中查找
+            list = (List<TbContent>) redisTemplate.boundHashOps(REDIS_CONTENT).get(categoryId);
+            if (list != null) {
+                return list;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
         Example example = new Example(TbContent.class);
         Example.Criteria criteria = example.createCriteria();
 
-        criteria.andEqualTo("status","1");
+        criteria.andEqualTo("categoryId", categoryId);
 
+        //启用状态的
+        criteria.andEqualTo("status", "1");
+
+        //降序排序
         example.orderBy("sortOrder").desc();
 
-        List<TbContent> list = contentMapper.selectByExample(example);
+        list = contentMapper.selectByExample(example);
+
+        try {
+            //设置某个分类对应的广告内容列表到缓存中
+            redisTemplate.boundHashOps(REDIS_CONTENT).put(categoryId, list);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return list;
     }
 
+    @Override
+    public void add(TbContent tbContent) {
+        super.add(tbContent);
+        //更新内容分类对应在redis中的内容列表缓存
+        updateContentInRedisByCategoryId(tbContent.getCategoryId());
+    }
+
+    private void updateContentInRedisByCategoryId(Long categoryId) {
+        try {
+            redisTemplate.boundHashOps(REDIS_CONTENT).delete(categoryId);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void update(TbContent tbContent) {
+        TbContent oldContent = super.findOne(tbContent.getId());
+
+        super.update(tbContent);
+
+        //是否修改了内容分类，如果修改了内容分类则需要将新旧分类对应的内容列表都得更新
+        if (!oldContent.getCategoryId().equals(tbContent.getCategoryId())) {
+            updateContentInRedisByCategoryId(oldContent.getCategoryId());
+        }
+        updateContentInRedisByCategoryId(tbContent.getCategoryId());
+    }
+
+    @Override
+    public void deleteByIds(Serializable[] ids) {
+        Example example = new Example(TbContent.class);
+
+        //1.根据内容id集合查询内容列表，然后在更新该内容分类对应的内容列表缓存
+        example.createCriteria().andIn("id", Arrays.asList(ids));
+
+        List<TbContent> contentList = contentMapper.selectByExample(example);
+
+        if (contentList != null && contentList.size() > 0) {
+            for (TbContent content : contentList) {
+                updateContentInRedisByCategoryId(content.getCategoryId());
+            }
+        }
+        //2.删除内容
+        super.deleteByIds(ids);
+    }
 }
